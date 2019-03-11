b500e75164f2f694caa99094fadcd36c4c1ec666
Mday1313 <melissaday076@gmail.com>
2019-03-11 14:23:40 -0400

    html and css added. FIX win count, GR countdown, pic, start new game when win.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -12,16 +12,18 @@ var currentWord = wordChoices[Math.floor(Math.random() * wordChoices.length)];
 var currentWordIndex = Array.from(currentWord);
 
 // set variables to hold event guesses wrong and correct
-var wrongGuesses = [];
+
+
+var wrongLetters = [];
 var lettersInWord = [];
 
  // Connecting js with html printout
 
  var directionsText = document.getElementById("directions-text");
- var wrongGuessesText = document.getElementById("lettersguessed-text");
+ var wrongGuessesText = document.getElementById("wrongGuesses");
  var currentWordText = document.getElementById("currentword-text");
  var winsText = document.getElementById("wins-text");
- var tiesText = document.getElementById("ties-text");
+ var guessesRemainingText = document.getElementById("guessesremaining-text");
  var lossesText = document.getElementById("losses-text");
 
 
@@ -31,10 +33,19 @@ function startGame() {
     for (i = 0; i < currentWord.length; i++) {
       lettersInWord[i] = "__";
     }
-    
+    guessesRemaining = 10;
     document.getElementById("currentword-text").innerHTML = lettersInWord.join(" ");
-   console.log(currentWord);
+    document.getElementById("guessesremaining-text").innerHTML = "Guesses Remaining: " + guessesRemaining;
+    document.getElementById("wrongGuesses").innerHTML = (" ");
+    document.getElementById("wins-text").innerHTML = "Wins: " + wins;
+    document.getElementById("losses-text").innerHTML = "Losses: " + losses;
+    console.log(currentWord);
+
   }
+
+  function updateImage() {
+    document.getElementById("airlock-image").src ="../images/airlock" + (guessesRemaining) + ".jpg";
+};
   
   function lettersGuessed() {
     document.onkeyup = function(event) {
@@ -47,10 +58,28 @@ function startGame() {
               lettersInWord[i] = userGuess;
                 document.getElementById("currentword-text").innerHTML = lettersInWord.join(" ");
                 found = true;
-            }
+            
+                }
+            }   
+        
+        if (found) return;
+
+        if (wrongLetters.indexOf(userGuess) < 0) {
+          wrongLetters.push(userGuess);
+          document.getElementById("wrongGuesses").innerHTML = wrongLetters.join(" ");
+          guessesRemaining;
+          updateImage();
+          document.getElementById("guessesremaining-text").innerHTML = "Guesses Remaining: " + guessesRemaining;
+          console.log(guessesRemaining);
+        }
+        if (guessesRemaining <= 0) {
+          losses++;
+          lossesText.textContent = losses + 1;
+          startGame();
         }
       }
-      }
+    }
+     
   
  
   
